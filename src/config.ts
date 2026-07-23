91d7e3f9e12e8105c8da1a2c95e19e22b415869d
Kanta IMAI <work@floating-gate.com>
2026-07-24 03:32:56 +0900

    260724-2

--- src/config.ts
+++ src/config.ts
@@ -2,13 +2,13 @@ export const SITE = {
 	title: 'Floating-Gate.com',
 	description:
 		'電子回路とソフトウェアを行き来しながら、作ったものと学んだことを記録する個人ブログ。',
-	author: 'jinji',
+	author: 'Kanta IMAI',
 };
 
 export const PROFILE = {
 	introduction:
 		'電子工学を学びながら、回路、組み込みシステム、ソフトウェア、Linuxなどを触っています。作ったものや、途中で考えたことを記録しています。',
-	github: 'https://github.com/jinji',
+	github: 'https://github.com/canta-9142',
 };
 
 export const NAV_ITEMS = [
